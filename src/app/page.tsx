429c4665e5bae8e87c82c6682cebf4560dbcee91
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2026-01-10 07:48:12 +0700

    refactor: increase max width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Top from "@/components/Top/page";
 export default function Home() {
   return (
     <div className="flex flex-col bg-white font-sans dark:bg-[#0a0a0a]">
-      <main className="flex min-h-screen w-full max-w-2xl flex-col mx-auto pt-18 pb-12 px-16 bg-white dark:bg-[#0a0a0a] sm:items-start gap-5">
+      <main className="flex min-h-screen w-full max-w-175 flex-col mx-auto pt-18 pb-12 px-16 bg-white dark:bg-[#0a0a0a] sm:items-start gap-5">
         <ModeToggle />
         <Top />
         <Links />
