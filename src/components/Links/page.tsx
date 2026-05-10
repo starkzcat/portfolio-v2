5aee6c50ee6ebc5e719f932ee64c666ab567e005
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2026-05-10 20:55:45 +0700

    chore: add cv & update socials links

--- src/components/Links/page.tsx
+++ src/components/Links/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { FileUser, Github, Linkedin, Mail } from 'lucide-react'
+import Link from "next/link"
 
 export default function Links() {
   return (
     <div className="flex items-center gap-5">
-      <Button><FileUser />Resume</Button>
-      <Github />
-      <Linkedin />
-      <Mail />
+
+      <Link href={"/cv.pdf"} target="_blank"><Button><FileUser />Resume</Button></Link>
+      <Link href={"https://github.com/starkzcat/"}><Github /></Link>
+      <Link href={"https://www.linkedin.com/in/andy-firmansyah/"}><Linkedin /></Link>
+      <Link href={"mailto:andyfirmansyah2667@gmail.com"}><Mail /></Link>
     </div>
   )
 }
